668fca37fae01f9ce876a9f9b5d7b18a1db57f07
Aditya Mathur <adiasfreelancer@gmail.com>
2024-05-16 16:42:15 +0530

    testimonial video

--- js/indexPage.js
+++ js/indexPage.js
@@ -31,3 +31,33 @@ setInterval(showNextArticle, 5000); // Change article every 3 seconds
 }
 
 startCarousel();
+
+
+
+
+
+jQuery(document).ready(function($) {
+    "use strict";
+    //  TESTIMONIALS CAROUSEL HOOK
+    $('#customers-testimonials').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots:true,
+        autoplayTimeout: 3000,
+        smartSpeed: 450,
+        responsive: {
+          0: {
+            items: 1
+          },
+          768: {
+            items: 2
+          },
+          1170: {
+            items: 3
+          }
+        }
+    });
+});
