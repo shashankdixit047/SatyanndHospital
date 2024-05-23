b7b08dc58bc91da644ce6e0630af6fdaaf6e1c1c
Aditya Mathur <adiasfreelancer@gmail.com>
2024-05-23 18:01:58 +0530

    Feedback form is Activate for all infomationed doctors

--- js/forms.js
+++ js/forms.js
@@ -1,1 +1,30 @@
-document.addEventListener("DOMContentLoaded",function(){document.getElementById("experience-form").addEventListener("submit",function(event){event.preventDefault();var form=event.target;var formData=new FormData(form);document.getElementById('alertMessage').innerText="Sending Experience...";document.getElementById('alertMessage').style.display="block";fetch(form.action,{method:'POST',body:formData}).then(response=>{if(response.ok){document.getElementById('alertMessage').innerText="Experience Shared Successfully!";setTimeout(function(){form.reset();document.getElementById('alertMessage').innerText="Your Experience Pending Review for Website."},5000)}else{document.getElementById('alertMessage').innerText="Failed to send message"}}).catch(error=>{console.error('Error:',error);document.getElementById('alertMessage').innerText="Something Went Wrong: "+error})})})
+document.addEventListener("DOMContentLoaded", function() {
+    var forms = document.querySelectorAll(".experience-form");
+    forms.forEach(function(form) {
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+            var formData = new FormData(form);
+            var alertMessage = form.querySelector('.alertMessage');
+            alertMessage.innerText = "Sending Experience...";
+            alertMessage.style.display = "block";
+            fetch(form.action, {
+                method: 'POST',
+                body: formData
+            }).then(response => {
+                if (response.ok) {
+                    alertMessage.innerText = "Experience Shared Successfully!";
+                    setTimeout(function() {
+                        form.reset();
+                        alertMessage.innerText = "Your Experience Pending Review for Website.";
+                    }, 5000);
+                } else {
+                    
+                    alertMessage.innerText = "Failed to send message";
+                }
+            }).catch(error => {
+                console.error('Error:', error);
+                alertMessage.innerText = "Something Went Wrong: " + error;
+            });
+        });
+    });
+});
